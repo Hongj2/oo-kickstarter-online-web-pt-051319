17d3a764e189e19341bed622d0c59af98162f08d
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-06-10 16:03:35 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,23 +1,16 @@
 class Project
   attr_accessor :title
-<<<<<<< HEAD
  attr_reader :backers
-=======
-  attr_reader :backers
->>>>>>> 36ec8761af0b30b0fde172b44b4eaefc2bd3878e
+
   
   def initialize (title)
     @title = title
     @backers = []
-<<<<<<< HEAD
-  end
+end
   
   def add_backer(backer_incharge_of_project)
     @backers << backer_incharge_of_project
      backer_incharge_of_project.backed_projects << self
-=======
-    
->>>>>>> 36ec8761af0b30b0fde172b44b4eaefc2bd3878e
   end
 end
     
