0e4661bd2cbab75b9cc0a055528b2269c15fcaaf
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-06-10 16:03:10 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,14 +7,15 @@ class Backer
     @backed_projects = []
   end
   
-<<<<<<< HEAD
+
   def back_project(projects_that_backer_is_incharge_of)
  @backed_projects << projects_that_backer_is_incharge_of
   projects_that_backer_is_incharge_of.backers << self
-=======
+end
+
   def back_project(project)
     @backed_projects << Project.new(project)
->>>>>>> 36ec8761af0b30b0fde172b44b4eaefc2bd3878e
+
   end
 end
     
